8c5e1ac95aa7fa55c894fc94babbad93683f296d
Alejandro <aleperaltabazas@hotmail.com>
2021-04-23 22:47:18 -0300

    delete sprite-dump

--- sprites-dump/README.md
+++
@@ -1,3 +0,0 @@
-# Sprites dump
-
-Script to crawl through https://www.pokencyclopedia.info to download both icon and front sprites for each pokemon on a given generation.
--- sprites-dump/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=$PWD
-
-sudo cp dex-sprites-dump "/usr/local/bin/dex-sprites-dump"
-sudo chmod +x "/usr/local/bin/dex-sprites-dump"
-sudo sed -i --expression "s@<dex-sprites-dump-root>@$DIR@g" /usr/local/bin/dex-sprites-dump
